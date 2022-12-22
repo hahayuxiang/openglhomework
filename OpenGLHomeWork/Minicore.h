088b46148cd246a41ddc4fbd0cea4ddcd799c163
hahayuxiang <hahayuxiang@qq.com>
2022-12-22 16:47:16 +0800

    修改长宽比

--- OpenGLHomeWork/Minicore.h
+++ OpenGLHomeWork/Minicore.h
@@ -2,8 +2,8 @@
 #include <GL/glew.h>
 //宏定义输出错误信息
 #define ERROR_LOG(ErrorMessage) std::cout << ErrorMessage << std::endl;glfwTerminate();exit(-1);
-#define WINDOWWIDTH 1080
-#define WINDOWHEIGHT 810
+#define WINDOWWIDTH 1500
+#define WINDOWHEIGHT 1000
 
 typedef void(*CheckStatus)(GLuint, GLenum, GLint*);
 typedef void(*ReadLog)(GLuint, GLsizei, GLsizei*, GLchar*);
